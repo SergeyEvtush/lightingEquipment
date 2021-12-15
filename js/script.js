8b551dede45f3ae8bc3d89aac57c2893652ec3b7
SergeyEvtush <evtushru208@gmail.com>
2021-12-15 17:57:10 +0200

    add slider slick and adaptive

--- js/script.js
+++ js/script.js
@@ -5,7 +5,21 @@ arrows:true,
 dots:true,
 infinite: true,
  slidesToShow: 2,
-
-
-});
+responsive:[
+	{
+	breakpoint: 690,
+	settings:{
+		slidesToShow: 1	
+					},
+	},
+	{
+		breakpoint: 520,
+		settings:{
+			arrows:false,
+			slidesToShow: 1		
+						},
+		}
+] 
+				
+									})
 });
