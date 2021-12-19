fafac5df8a060cec6b2e06c924d789e2fddbd8b3
SergeyEvtush <evtushru208@gmail.com>
2021-12-20 01:04:10 +0200

    Не решен вопрос с скролом меню

--- js/script.js
+++ js/script.js
@@ -38,3 +38,17 @@ $('.rentequipment__slider').slick({
 									});
 								
 });
+//бургер
+const iconMenu=document.querySelector('.menu__icon');
+const menuBody=document.querySelector('.menu__list');
+if(iconMenu)
+{
+
+	//при клике на menu__icon то есть на бургер(на саму иконку) присваиваем body класс _lock,а для иконки и для самого меню
+	//добавляем им класс _active
+iconMenu.addEventListener("click",function(e){
+	document.body.classList.toggle('_lock');	
+iconMenu.classList.toggle('_active');
+menuBody.classList.toggle('_active');
+});
+}
