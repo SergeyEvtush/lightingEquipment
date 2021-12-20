b54e9811cdc7ba6d07a71f4df6786046cfe3756f
SergeyEvtush <evtushru208@gmail.com>
2021-12-21 00:56:27 +0200

    Final work,all bugs are fixed

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@ $('.rentequipment__slider').slick({
 });
 //бургер
 const iconMenu=document.querySelector('.menu__icon');
-const menuBody=document.querySelector('.menu__list');
+const menuBody=document.querySelector('.menu__body');
 if(iconMenu)
 {
 
@@ -52,3 +52,39 @@ iconMenu.classList.toggle('_active');
 menuBody.classList.toggle('_active');
 });
 }
+const menuLinks=document.querySelectorAll('.menu__link[data-goto]');//ищем только те элементы классов menu__link у котрых есть атрибут data-goto
+if(menuLinks.length>0)
+{
+menuLinks.forEach(menuLink=>{
+menuLink.addEventListener("click",onMenuLinkClick)
+});
+function onMenuLinkClick(e)
+	{
+const menuLink=e.target;
+//проверка на то  что есть ли чтото в датат атрибуте и  существует ли объект на который ссылается данный атрибут  
+if(menuLink.dataset.goto&&document.querySelector(menuLink.dataset.goto))
+			{
+	//получаем сам объект на который	 ссылается дата атрибут
+const gotoBlock = document.querySelector(menuLink.dataset.goto);
+//вычисляем положение объекта с учетом высоты шапки
+const gotoBlockValue=/*местоположение объекта от верха в пикселях*/gotoBlock.getBoundingClientRect().top+/*количество прокрученных пикселей*/pageYOffset-/*высота шапки*/document.querySelector('header').offsetHeight;
+/*проверка нужна для того чтобы при открытом меню при прокрутке к нужному разделу меню закрывалось */
+if(iconMenu.classList.contains('_active'))
+{
+	/*убираем классы которые добавили при открытии меню */
+	document.body.classList.remove('_lock');	
+iconMenu.classList.remove('_active');
+menuBody.classList.remove('_active');
+
+}
+
+/*код который прокручивает скролл на высоту равную константе  gotoBlockValue*/
+window.scrollTo
+	({
+top:gotoBlockValue,
+behavior:"smooth"/* плавность поркрутки*/
+		});
+e.preventDefault();//отключаем работу ссылки
+			}
+		}
+	}
